a7cec56f66e3c233fa190fe44dc58ec141d48396
Sajeed Syed <123931227+mrsajids@users.noreply.github.com>
2024-12-21 09:07:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Function to copy code to clipboard
-function copyCode() {
-  var codeBlock = document.getElementById("codeBlock");
+function copyCode1() {
+  var codeBlock = document.getElementById("codeBlock1");
   var range = document.createRange();
   range.selectNode(codeBlock);
   window.getSelection().removeAllRanges();
@@ -8,7 +8,24 @@ function copyCode() {
   document.execCommand("copy"); // Copy the selected content
 
   // Show notification after copy
-  var notification = document.getElementById("copiedNotification");
+  var notification = document.getElementById("copiedNotification1");
+  notification.style.display = "block";
+  setTimeout(function() {
+    notification.style.display = "none";
+  }, 2000); // Hide after 2 seconds
+}
+
+// Function to copy code to clipboard
+function copyCode2() {
+  var codeBlock = document.getElementById("codeBlock2");
+  var range = document.createRange();
+  range.selectNode(codeBlock);
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
+  document.execCommand("copy"); // Copy the selected content
+
+  // Show notification after copy
+  var notification = document.getElementById("copiedNotification2");
   notification.style.display = "block";
   setTimeout(function() {
     notification.style.display = "none";
